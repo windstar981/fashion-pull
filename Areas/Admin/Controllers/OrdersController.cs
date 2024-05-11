fd4b6705da268a71de8f03e768513a94cb9437f6
nmhieu <hackviet98@gmail.com>
2024-05-12 02:27:40 +0700

    up code

---
+++ Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using fashion.Data;
+using fashion.Services;
+using fashion.Areas.Admin.Models;
+
+namespace fashion.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrdersController : Controller
+    {
+        private readonly FashionContext _context;
+
+        public OrdersController(FashionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Orders
+        public async Task<IActionResult> Index()
+        {
+            if (HttpContext.Session.GetInt32("IdUser") == null)
+            {
+                return Redirect("/Admin");
+            }
+            var orders = _context.Orders.Include(o => o.Customer);
+
+            return View(await orders.ToListAsync());
+        }
+
+        // GET: Admin/Orders/Order_Status
+        public async Task<IActionResult> OrderStatus()
+        {
+            var fashionContext = _context.Orders.Include(o => o.Customer);
+            return View(/*await fashionContext.ToListAsync()*/);
+        }
+
+        // GET: Admin/Orders/Order_Invoice
+        public async Task<IActionResult> Order_Invoice()
+        {
+            var fashionContext = _context.OrderDetails.Include(o => o.Order).Include(o => o.Product);
+            return View(/*await fashionContext.ToListAsync()*/);
+        }
+
+        // GET: Admin/Orders/Order_Notes
+        public async Task<IActionResult> Order_Notes()
+        {
+            var fashionContext = _context.Orders.Include(o => o.Customer);
+            return View(/*await fashionContext.ToListAsync()*/);
+        }
+
+        // GET: Admin/Orders/Order_Messages
+        public async Task<IActionResult> Order_Messages()
+        {
+            var fashionContext = _context.Orders.Include(o => o.Customer);
+            return View(/*await fashionContext.ToListAsync()*/);
+        }
+
+        // GET: Admin/Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Customer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TotalPrice,Note,Fee,CreatedBy,CreatedAt,UpdatedBy,UpdatedAt,CustomerId,OrderDate")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", order.CustomerId);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", order.CustomerId);
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TotalPrice,Note,Fee,CreatedBy,CreatedAt,UpdatedBy,UpdatedAt,CustomerId,OrderDate")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", order.CustomerId);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Customer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fashion.Data;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? Pd { get; set; }
+}
